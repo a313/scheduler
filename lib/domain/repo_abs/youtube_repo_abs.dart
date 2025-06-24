1927e045138d2886d6fb00979a96ec7064703e17
Theon <nguyenthanhtung0207@gmail.com>
2025-06-24 21:44:25 +0700

    rm unused files

--- lib/domain/repo_abs/youtube_repo_abs.dart
+++
@@ -1,7 +0,0 @@
-import 'package:scheduler/core/usecase/data_state.dart';
-import 'package:youtube_explode_dart/youtube_explode_dart.dart';
-
-abstract class YoutubeRepo {
-  Future<DataState<Video>> getVideoFromId(String id);
-  Future<DataState<List<String>>> getDownloadUrl(String id);
-}
